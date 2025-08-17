cd62a1ed2aebd0b85ecfda497e8fb9d9607f6fdd
Long32 <99838967+Long132000@users.noreply.github.com>
2025-08-17 16:58:01 +0300

    Add files via upload

---
+++ ♠ИСПО♠/мусор 🗑/лабораторная 1/ConsoleApplication3/Задание 4.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+
+	int sum = x + y;
+	int raz = x - y;
+	int pro = x * y;
+	int otn = x / y;
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Сумма x+y=" << sum << endl;
+	cout << "Разность x-y=" << raz << endl;
+	cout << "Произведение x*y=" << pro << endl;
+	cout << "Отношение x/y=" << otn << endl;
+
+	return 0;
+}
