90c5eee908cd8571fb0c773690b5cdf20645cb8f
Long32 <99838967+Long132000@users.noreply.github.com>
2025-08-17 17:09:40 +0300

    Add files via upload

---
+++ ♠ИСПО♠/ОАП/ОАП я ☜(ﾟヮﾟ☜)/2семестр.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	const int N = 5;
+	int arr[10];
+	int arrl[N];
+	for (int i = 0; i < 10; i++) {
+		arr[i] = 20 + rand() % (100 - 20);
+
+		cout << arr[i] << " ";
+	}
+    std::cout << "Hello World!\n";
+}
