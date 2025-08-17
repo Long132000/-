bc7dfcf128020755a6e1643858ccb022bab91664
Long32 <99838967+Long132000@users.noreply.github.com>
2025-08-17 16:39:35 +0300

    Add files via upload

---
+++ ♠ИСПО♠/МДК ⛔/МДК. 01.01. РПМ/МДК.01.01/mdk 0101/Lab3/Lab3-2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+public class Arrays
+{
+    public static void InputArray(int[] arr)
+    {
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    public static void OutputArray(int[] arr)
+    {
+        Console.WriteLine("Массив:");
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public static int CountMaxElements(int[] arr)
+    {
+        if (arr.Length == 0) return 0;
+        int max = arr[0];
+        int count = 1;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                count = 1;
+            }
+            else if (arr[i] == max)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void OutputArrayWithColoredMax(int[] arr)
+    {
+        int max = arr.Max();
+        int count = arr.Count(x => x == max);
+
+        Console.WriteLine("Массив с выделенными максимальными элементами:");
+        foreach (int num in arr)
+        {
+            if (num == max && count > 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(num + " ");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write(num + " ");
+            }
+        }
+        Console.WriteLine();
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер первого массива:");
+        int size1 = int.Parse(Console.ReadLine());
+        int[] arr1 = new int[size1];
+        InputArray(arr1);
+        OutputArray(arr1);
+        Console.WriteLine($"Количество максимальных элементов: {CountMaxElements(arr1)}");
+        OutputArrayWithColoredMax(arr1);
+
+
+        Console.WriteLine("Введите размер второго массива:");
+        int size2 = int.Parse(Console.ReadLine());
+        int[] arr2 = new int[size2];
+        InputArray(arr2);
+        OutputArray(arr2);
+        Console.WriteLine($"Количество максимальных элементов: {CountMaxElements(arr2)}");
+        OutputArrayWithColoredMax(arr2);
+
+
+        Console.WriteLine("Введите размер третьего массива:");
+        int size3 = int.Parse(Console.ReadLine());
+        int[] arr3 = new int[size3];
+        InputArray(arr3);
+        OutputArray(arr3);
+        Console.WriteLine($"Количество максимальных элементов: {CountMaxElements(arr3)}");
+        OutputArrayWithColoredMax(arr3);
+
+    }
+}
---
+++ ♠ИСПО♠/МДК ⛔/МДК. 01.01. РПМ/МДК.01.01/mdk 0101/Lab3/Lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Parallelogram
+{
+    public static double CalculatePerimeter(double a, double b)
+    {
+        return 2 * (a + b);
+    }
+
+    public static double CalculateArea(double a, double b, double angle)
+    {
+        return a * b * Math.Sin(angle * Math.PI / 180); // Преобразование градусов в радианы
+    }
+
+    public static double CalculateDiagonal(double a, double b, double angle)
+    {
+        return Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos(angle * Math.PI / 180));
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину стороны a:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите длину стороны b:");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите угол при основании (в градусах):");
+        double angle = double.Parse(Console.ReadLine());
+
+        double perimeter = CalculatePerimeter(a, b);
+        double area = CalculateArea(a, b, angle);
+        double diagonal = CalculateDiagonal(a, b, angle);
+
+        Console.WriteLine($"Периметр: {perimeter}");
+        Console.WriteLine($"Площадь: {area}");
+        Console.WriteLine($"Длина диагонали: {diagonal}");
+    }
+}
