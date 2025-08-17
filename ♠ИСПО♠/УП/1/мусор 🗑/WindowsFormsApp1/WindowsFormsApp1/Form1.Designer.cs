7471eaddd51e75b507ff7575d8b5d1e24c890fde
Long32 <99838967+Long132000@users.noreply.github.com>
2025-08-17 17:43:55 +0300

    Add files via upload

---
+++ ♠ИСПО♠/УП/1/мусор 🗑/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button666 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(529, 159);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.Click += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(497, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(622, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(612, 345);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "зелёный";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(612, 304);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "синий";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(612, 262);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "красный";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "размер";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button666
+            // 
+            this.button666.Location = new System.Drawing.Point(436, 280);
+            this.button666.Name = "button666";
+            this.button666.Size = new System.Drawing.Size(75, 23);
+            this.button666.TabIndex = 12;
+            this.button666.Text = "сохранить";
+            this.button666.UseVisualStyleBackColor = true;
+            this.button666.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "чтение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button666);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button666;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ♠ИСПО♠/УП/1/мусор 🗑/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int _clr = -1;
+        private string _str;
+        const string userRoot = "HKEY_CURRENT_USER";
+        const string subkey = "RegistrySetValueExample";
+        const string keyName = userRoot + "\\" + subkey;
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if ((int)Registry.GetValue(keyName, "clr", "-1") == 0)
+                Collor(Color.DarkSlateGray, Color.Gray);
+            if ((int)Registry.GetValue(keyName, "clr", "-1") == 1)
+                Collor(Color.WhiteSmoke, Color.White);
+            maskedTextBox1.Text = (string)Registry.GetValue(keyName, "text", "");
+            Size = new Size((int)Registry.GetValue(keyName, "y", "808"), (int)Registry.GetValue(keyName, "x", "477"));
+        }
+        private void button1_Click(object sender, EventArgs e)//DeepPink цвет
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+
+            key.SetValue("Color", "DeepPink");
+            key.Close();
+            BackColor = System.Drawing.Color.DeepPink;
+        }
+        private void button2_Click(object sender, EventArgs e)//DarkBlue цвет
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+
+            key.SetValue("Color", "DarkBlue");
+            key.Close();
+            BackColor = System.Drawing.Color.DarkBlue;
+        }
+        private void button3_Click(object sender, EventArgs e)//PaleVioletRed цвет
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+
+            key.SetValue("Color", "PaleVioletRed");
+            key.Close();
+            BackColor = System.Drawing.Color.PaleVioletRed;
+        }
+        private void button4_Click(object sender, EventArgs e)//размер окна
+        {
+            string hght = textBox2.Text;
+            string wdth = textBox3.Text;
+
+            this.Height = Convert.ToInt32(hght);
+            this.Width = Convert.ToInt32(wdth);
+        }
+        private void button5_Click(object sender, EventArgs e)//чтение из реестра и вывод на экран
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+
+            if (key != null)
+            {
+                int Height = int.Parse(key.GetValue("Height").ToString());
+                int Width = int.Parse(key.GetValue("Width").ToString());
+                this.Size = new Size(Width, Height);
+
+
+                string color = (string)key.GetValue("Color");
+                if (color == "Pink")
+                    BackColor = System.Drawing.Color.Pink;
+                else if (color == "IndianRed")
+                    BackColor = System.Drawing.Color.IndianRed;
+                if (color == "White")
+                    BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void button6_click(object sender, EventArgs e)//сохранение
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Deniska");
+
+            key.SetValue("Height", this.Height);
+            key.SetValue("Width", this.Width);
+            key.Close();
+        }
+        private void Collor(Color buttonc, Color backgroundc)
+        {
+            button1.BackColor = buttonc;
+            button2.BackColor = buttonc;
+            button3.BackColor = buttonc;
+            listBox1.BackColor = buttonc;
+            this.BackColor = backgroundc;
+            maskedTextBox1.BackColor = buttonc;
+        }
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            _str = maskedTextBox1.Text;
+            Registry.SetValue(keyName, "text", _str);
+        }
+    }
+}
