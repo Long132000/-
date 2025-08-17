e674a588271ea494ad3fc8a7b1af58e3cfd7d5c2
Long32 <99838967+Long132000@users.noreply.github.com>
2025-08-17 17:04:46 +0300

    Add files via upload

---
+++ ♠ИСПО♠/ОАП/ОАП я ☜(ﾟヮﾟ☜)/Лабораторная 1c/Лабораторная 1c/Лабораторная 1c.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+	double A = 8;
+	double B = 5.6;
+	double pi = 3.14;
+	double Y = 2 * A * asin(pi / B);
+	printf("Y = %f\n", Y);
+	return 0;
+}
