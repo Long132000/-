887eb1503bfeb5c8f3a0153528ceeb48646ca065
Long32 <99838967+Long132000@users.noreply.github.com>
2025-10-07 13:37:06 +0300

    Add files via upload

---
+++ ♠ИСПО♠/ИСРПО/Git_Projects/proectiki/MyUtilityPackage/MyUtilityPackage/InternalStringHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace MyUtilityPackage
+{
+    // Этот класс не будет виден за пределами сборки (пакета)
+    internal class InternalStringHelper
+    {
+        public static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ ♠ИСПО♠/ИСРПО/Git_Projects/proectiki/MyUtilityPackage/MyUtilityPackage/PublicCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace MyUtilityPackage
+{
+    public class PublicCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Этот метод будет доступен только внутри самой сборки (пакета)
+        internal int InternalMultiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
